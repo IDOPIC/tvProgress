bc5d774422e4a172b44cc3dcf992da7c0ff8e237
Cormery Antoine <antoine.cormery@epita.fr>
2016-05-04 21:03:27 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,10 @@
+tvProgress
+===================
+
+A tvOS Framework for progress HUD displaying.
+
+
+Install
+-------------
+
+Add the xcodeproject on your project
---
+++ tvProgress/tvProgress/tvProgress.h
@@ -0,0 +1,19 @@
+//
+//  tvProgress.h
+//  tvProgress
+//
+//  Created by Antoine Cormery on 04/05/2016.
+//  Copyright © 2016 Antoine Cormery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for tvProgress.
+FOUNDATION_EXPORT double tvProgressVersionNumber;
+
+//! Project version string for tvProgress.
+FOUNDATION_EXPORT const unsigned char tvProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tvProgress/PublicHeader.h>
+
+
