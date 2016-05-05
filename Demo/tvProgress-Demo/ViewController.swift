5ff822a8151c7dd2cd15b0b4ee578c548b7c2b82
Cédric Eugeni <cedric.eugeni@gmail.com>
2016-05-05 16:06:13 +0200

    Created framework project and demo project

---
+++ Demo/tvProgress-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tvProgress-Demo
+//
+//  Created by Cédric Eugeni on 05/05/2016.
+//  Copyright © 2016 tvProgress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ tvProgress/tvProgress.h
@@ -0,0 +1,19 @@
+//
+//  tvProgress.h
+//  tvProgress
+//
+//  Created by Cédric Eugeni on 05/05/2016.
+//  Copyright © 2016 tvProgress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for tvProgress.
+FOUNDATION_EXPORT double tvProgressVersionNumber;
+
+//! Project version string for tvProgress.
+FOUNDATION_EXPORT const unsigned char tvProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tvProgress/PublicHeader.h>
+
+
